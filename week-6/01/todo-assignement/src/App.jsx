388deccf6210eef41217142226024d07452d7a9b
SAURABH DWIVEDI <saurabhdwivedi2310@gmail.com>
2024-03-20 22:43:29 +0530

    Learnt useEffect and fetching data from backend under useEffect

--- week-6/01/todo-assignement/src/App.jsx
+++ week-6/01/todo-assignement/src/App.jsx
@@ -1,43 +1,60 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import CardWrapper from './components/CardWrapper';
-import TextComponent from './components/TextComponent';
 
-// Now we will see : How to render component as a prop { jaise title as a prop bhjte the yaha pe ek component ko hi as prop bhjege}
-// cardWrapper k andar ka job h , sab as a children prop pass ho jyega iske andar jo b yha likha h sab wha access ho jyega
-function App(){
+/* Understanding of useEffect , what is dependency array in ueEffect hook.
+ This empty array is a dependency  if empty then it will re-render only once as updates,
+     but if given some value then let say [counter] then it will update each time counter update. */
+
+// Remember first arguement of useEffect can not be async so use promis , .then syntax
+function App() {
+
+    const[todos, setTodos] = useState([]);
+
+    useEffect(()=>{
+           setInterval(()=>{
+            fetch("https://sum-server.100xdevs.com/todos")
+            .then( async function(res){
+                const json = await res.json();
+                setTodos(json.todos);
+            })
+           },3000)
+    },[])
+
+    // simple way , above is refreshing after 3 seconds
+
+    // useEffect(()=>{
+    //         fetch("https://sum-server.100xdevs.com/todos")
+    //         .then( async function(res){
+    //             const json = await res.json();
+    //             setTodos(json.todos);
+    //         })
+    // },[])
+    
     return <div>
-        <CardWrapper>
-            <div>Hello Motto!</div>
-            <TextComponent/>
-        </CardWrapper>
-        <CardWrapper>
-            <div>Hello SEEnU!</div>
-        </CardWrapper>
+         {todos.map(todo => <Todo title={todo.title} description={todo.description} key={todo.id}/>)}
     </div>
 }
 
-
-// function App() {
-//     return (
-//         <div>
-//            <CardWrapper innerComponent={<TextComponent/>}/>
-//            <CardWrapper innerComponent={<TextComponent/>}/>
-//         </div>
-//     );
-// }
-
-// function CardWrapper({innerComponent}) {
-//     return <div style={{ border : "2px solid black" , padding: "10px" , margin:"20px" }}>
-//         {innerComponent}
-//       </div>
-//   }
-  
-// function TextComponent(){
-//   return <div>
-//        <h1> Hello </h1>
-//     </div>
-// }
-
+function Todo({title , description}){
+     return <div>
+        <h1>{title}</h1>
+        <h1>{description}</h1>
+    </div>
+}
 
 export default App;
+
+
+
+/*
+
+Things to cover in week-6.0
+    -React Returns
+    -Re-rendering (Memoization)
+    -key
+    -Wrapper Component
+    -useEffect
+    -useMemo
+    -useCallback
+    -useRef
+*/
--- week-6/01/todo-assignement/src/components/TextComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-export default function TextComponent(){
-    return <div style={{border: "2px solid black",padding: "10px",margin: "20px"}}>
-         <h1> Hello </h1>
-      </div>
-  }
