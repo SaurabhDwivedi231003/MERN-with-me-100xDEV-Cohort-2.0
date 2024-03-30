6551e229ccad166b4dc3903941d2e0cf881c2098
SAURABH DWIVEDI <saurabhdwivedi2310@gmail.com>
2024-03-30 23:01:16 +0530

    Understading useCallback

--- week-6/02/week-6.2/src/App.jsx
+++ week-6/02/week-6.2/src/App.jsx
@@ -1,67 +1,94 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState , memo, useCallback } from 'react';
 import './App.css';
 
-                           /* USE MEMO */
-
-/*   => useMemo  : Create an app that does two thing :
-          -> Increase a counter by one.
-          -> Lts user put a value in an input box (n) , and you need to show sum from 1 to n.
-
-        Condition : everything should to be inside app.
-*/
+/* useCallback : used to avoid rerender for refrencial things */
+/* make functions a variable , wrapped inside useCallback */
+/* Memo use hoga useCallback k sath*/
 
 function App() {
 
-  const [counter , setCounter] = useState(0);
-  const [inputValue , setInputValue] = useState(0);
-  // const [finalValue , setFinalValue] = useState(0);  // used for useEffect
+const [counter , setCounter] = useState(0);
 
-  /* Brute and ineffecient way to do , bad practice 
-      let count = 0;
-      for(let i=1 ; i<=inputValue ; i++){
-        count += i;
-      }
-  */
+//  function inputFunction(){
+//       console.log("Child Clicked");
+//   }
 
-  /* In this above approach everytime , we increase the counter entire page rerenders
-       and the sum from 1 to n is calculated again and again.
-    To avoid this we can use decent approach of useEffect hook.
-    => But best approach is to use useMemo hook.
-  */
+/* Memo : lets you skip rerendering a component when its props are unchanged */
+// But yha pe fir bhi child rerender ho rha h , why??
+// Beacause : 
+/*
+    function inputFunction1(){ console.log("Hi")}
+    function inputFunction2(){ console.log("Hi")}
 
- /*Using useEffect 
-   useEffect(()=>{ 
-      let count = 0;
-      for(let i=1 ; i<=inputValue ; i++){
-        count += i;
-      }
-      setFinalValue(count);
-    }, [inputValue]); 
-   */ 
+    inputFunction == inputFunction2 , javascript returns false even if their body their content is same.
+    and since it consider them as diffrent function it RE-RENDERS.
+    To avoid this , we use : useCallback
+*/
 
-  /*Using useMemo : more cleaner way and do no require extra useState : finalValue*/
+/* writing inputFunction() : same function in this way */
+const inputFunction = useCallback(()=>{
+      console.log("Child Clicked")
+},[])    // Again this is a depedency array , depends on conditions on which we want it to allow for rerendering
 
-  let count = useMemo(()=>{ 
-    let finalCount = 0;
-    for(let i=1 ; i<=inputValue ; i++){
-      finalCount += i;
-    }
-    return finalCount;
-  }, [inputValue]);
 
 
 return (
     <>
-     <input placeholder='enter a number' onChange={function(e){
-        setInputValue(e.target.value);
-     }}/>
-     <br/>
-     Sum from 1 to {inputValue} = {count}
-
      <button onClick={()=> {setCounter(counter+1)}}> Counter : {counter}</button>
+     <Child inputFunction={inputFunction}/>
     </>
   );
 }
 
+const Child = memo(({inputFunction}) => {
+      console.log("Child renders")
+
+      return <>
+            <button onClick={inputFunction}>Button Clicked</button>
+      </>
+});
+
 
 export default App;
+
+
+
+/* Memo : lets you skip rerendering a component when its props are unchanged */
+// But yha pe fir bhi child rerender ho rha h , why??
+// Beacause : 
+/*
+    function inputFunction1(){ console.log("Hi")}
+    function inputFunction2(){ console.log("Hi")}
+
+    inputFunction == inputFunction2 , javascript returns false even if their body their content is same.
+    and since it consider them as diffrent function it RE-RENDERS.
+    To avoid this , we use : useCallback
+*/
+
+
+// function App() {
+
+// const [counter , setCounter] = useState(0);
+
+// function logSomething(){
+//     console.log("Child Clicked");
+// }
+
+// return (
+//     <>
+//      <button onClick={()=> {setCounter(counter+1)}}> Counter : {counter}</button>
+//      <Child inputFunction={logSomething}/>
+//     </>
+//   );
+// }
+
+// const Child = memo(({inputFunction}) => {
+//       console.log("Child renders")
+
+//       return <>
+//             <button onClick={inputFunction}>Button Clicked</button>
+//       </>
+// });
+
+
+// export default App;
