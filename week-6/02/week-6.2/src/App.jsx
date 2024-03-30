8a0163b1b7f4c1ab1b7887363decec4b9ce5d0e2
SAURABH DWIVEDI <saurabhdwivedi2310@gmail.com>
2024-03-30 20:20:30 +0530

    useMemo

--- week-6/02/week-6.2/src/App.jsx
+++ week-6/02/week-6.2/src/App.jsx
@@ -1,46 +1,67 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-// Assignment : take id from backend , FETCH data from bakend for same id and display that data on frontend.
+                           /* USE MEMO */
+
+/*   => useMemo  : Create an app that does two thing :
+          -> Increase a counter by one.
+          -> Lts user put a value in an input box (n) , and you need to show sum from 1 to n.
+
+        Condition : everything should to be inside app.
+*/
 
 function App() {
-  const [id, setId] = useState('65ead2a66517fe444515d5e2');
 
-  return (
+  const [counter , setCounter] = useState(0);
+  const [inputValue , setInputValue] = useState(0);
+  // const [finalValue , setFinalValue] = useState(0);  // used for useEffect
+
+  /* Brute and ineffecient way to do , bad practice 
+      let count = 0;
+      for(let i=1 ; i<=inputValue ; i++){
+        count += i;
+      }
+  */
+
+  /* In this above approach everytime , we increase the counter entire page rerenders
+       and the sum from 1 to n is calculated again and again.
+    To avoid this we can use decent approach of useEffect hook.
+    => But best approach is to use useMemo hook.
+  */
+
+ /*Using useEffect 
+   useEffect(()=>{ 
+      let count = 0;
+      for(let i=1 ; i<=inputValue ; i++){
+        count += i;
+      }
+      setFinalValue(count);
+    }, [inputValue]); 
+   */ 
+
+  /*Using useMemo : more cleaner way and do no require extra useState : finalValue*/
+
+  let count = useMemo(()=>{ 
+    let finalCount = 0;
+    for(let i=1 ; i<=inputValue ; i++){
+      finalCount += i;
+    }
+    return finalCount;
+  }, [inputValue]);
+
+
+return (
     <>
-      <button onClick={() => setId('65ead2a66517fe444515d5e2')}>1</button>
-      <button onClick={() => setId('65ead2e26517fe444515d5e6')}>2</button>
-      <button onClick={() => setId('65f180e73cfdf203612f434a')}>3</button>
-      <button onClick={() => setId('65f181663cfdf203612f434c')}>4</button>
-      {/* Pass the id directly, not setId */}
-      <Todo id={id} />
+     <input placeholder='enter a number' onChange={function(e){
+        setInputValue(e.target.value);
+     }}/>
+     <br/>
+     Sum from 1 to {inputValue} = {count}
+
+     <button onClick={()=> {setCounter(counter+1)}}> Counter : {counter}</button>
     </>
   );
 }
 
-function Todo({ id }) {
-  const [todo, setTodo] = useState(null);
-
-  useEffect(() => {
-    axios.get(`http://localhost:3000/todos/${id}`)
-      .then(function (response) {
-        setTodo(response.data);
-      })
-  }, [id]); // most important line
-
-  return (
-    <div>
-      {/* Render todo details if it exists */}
-      {todo && (
-        <>
-          <h4>{todo.title}</h4>
-          <h5>{todo.description}</h5>
-          <p>Completed: {todo.completed ? 'Yes' : 'No'}</p>
-        </>
-      )}
-    </div>
-  );
-}
 
 export default App;
