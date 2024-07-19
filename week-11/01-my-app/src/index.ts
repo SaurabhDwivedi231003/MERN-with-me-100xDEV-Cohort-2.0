cb886445f230639b2726286e2621ab9c32cadf42
SAURABH DWIVEDI <saurabhdwivedi2310@gmail.com>
2024-07-19 23:49:38 +0530

    SERVERLESS FNS

---
+++ week-11/01-my-app/src/index.ts
@@ -0,0 +1,13 @@
+
+export interface Env {
+
+}
+
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
+}; 
